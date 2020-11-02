19f2195c4bfa1baaefb801adbdc616cb19ff9456
JoseFeliciano-spec <68482634+JoseFeliciano-spec@users.noreply.github.com>
2020-11-02 12:08:31 -0500

    Sistema de usuario y admin

--- src/components/SistemaPC/BottomBarPC/BottomBarPC.js
+++ src/components/SistemaPC/BottomBarPC/BottomBarPC.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
@@ -13,8 +13,10 @@ import { useHistory } from "react-router-dom";
 import "./BottomBarPC.scss";
 import RegistroPC from "../../../pages/SistemaPC/RegistroPC";
 import { ToastContainer } from "react-toastify";
+import firebase from '../../../utils/Firebase';
+import "firebase/firestore";
 
-
+const db = firebase.firestore(firebase);
 
 function BottomBarPC(props) {
   /* Const user pasado por props */
@@ -22,12 +24,23 @@ function BottomBarPC(props) {
   /* Abrir y cerrar el componente registro */
   const [open, setOpen] = useState(false);
 
+  /* Admin zone */
+  const [adminZone, setAdminZone] = useState(false);
+
   /* Ver el historial */
   let history = useHistory();
   const onClick = (e) => {
     history.push("/");
   };
 
+
+  /* Saber si el usuario es admin o no */
+  const existeAdmin = async()=>{
+    await db.collection("admin").doc(user.uid)
+    .onSnapshot(function(value){
+      setAdminZone(value.exists);
+    });
+  }
 
   /* Cambiar de barra */
   const handlerCambiar = (e) => {
@@ -47,6 +60,13 @@ function BottomBarPC(props) {
     setOpen(!open);
   };
 
+
+  /* Saber si siempre está el usuario es admin */
+  useEffect(() => {
+    existeAdmin();
+  }, [])
+
+
   return (
     <div>
       <AppBar position="fixed" color="primary" className="bottombarPC">
@@ -57,17 +77,21 @@ function BottomBarPC(props) {
             id="verBarraPC"
             icon={<VisibilityIcon />}
           />
-          <BottomNavigationAction
+          {adminZone&&(
+            <BottomNavigationAction
             label="Editar"
             id="editarBarraPC"
             onClick={handlerCambiar}
             icon={<CreateIcon />}
-          />
-          <BottomNavigationAction
+            />
+          )}
+          {adminZone&&(
+            <BottomNavigationAction
             label="Crear"
             onClick={handlerOpen}
             icon={<AddCircleIcon />}
           />
+          )}
         </BottomNavigation>
       </AppBar>
 
@@ -82,7 +106,9 @@ function BottomBarPC(props) {
         draggable
         pauseOnHover
       />
-      <RegistroPC open={open} user={user} setOpen={setOpen} />
+      {adminZone&&(
+        <RegistroPC open={open} user={user} setOpen={setOpen} />
+      )}
     </div>
   );
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy } from "react";
+import React, { useEffect, useState, Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import BottomBarHome from "../components/Home/BottomBarHome";
 import BottomBarPC from "../components/SistemaPC/BottomBarPC";
@@ -7,11 +7,31 @@ import Nosotros from "../pages/Home/Nosotros";
 import Inicio from "../pages/Home/Inicio";
 /* import RegistroPC from "../pages/SistemaPC/RegistroPC"; */
 import EditarPC from "../pages/SistemaPC/EditarPC";
+import firebase from "../utils/Firebase";
+import "firebase/firestore";
 
 const SistemaPC = lazy(()=>import("../pages/SistemaPC/VerPC"));
 
+const db = firebase.firestore(firebase);
+
 export default function Routes(props) {
   const { open, setOpen, user } = props;
+  const [adminZone, setAdmin] = useState(false);
+  /* console.log(user.uid); */
+
+
+  const existeAdmin = async ()=>{
+    const hola = await db.collection("admin").doc(user.uid)
+    .onSnapshot(function(value){
+      setAdmin(value.exists);
+    });
+    /* console.log(hola.exists); */
+  };
+
+  useEffect(() => {
+    existeAdmin();
+  }, [])
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
@@ -27,10 +47,14 @@ export default function Routes(props) {
           <SistemaPC />
           <BottomBarPC open={open} setOpen={setOpen} user={user} />
         </Route>
-        <Route path="/sistema-computo/editar" exact>
-          <EditarPC />
-          <BottomBarPC open={open} setOpen={setOpen} user={user} />
-        </Route>
+        {
+          adminZone&&(
+            <Route path="/sistema-computo/editar" exact>
+              <EditarPC />
+              <BottomBarPC open={open} setOpen={setOpen} user={user} />
+            </Route>
+          )
+        }
         <Route path="*">
           <h1>Error 404</h1>
         </Route>
